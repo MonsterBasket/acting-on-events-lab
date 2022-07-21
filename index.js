e3b150942b6d925b1d1e8a451175d4511120e051
MonsterBasket <blazza1@hotmail.com>
2022-07-21 23:16:05 +1000

    successfully passed 0 tests :D

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Your code here
+document.addEventListener("keydown", (event) => {
+if (event.key === 'ArrowLeft'){
+    moveDodger(-1)
+}
+else if (event.key === 'ArrowRight'){
+    moveDodger(1)
+}
+});
+
+function moveDodger(direction){
+    const leftNumbers = dodger.style.left.replace("px", "");
+    let left = parseInt(leftNumbers, 10);
+
+    if (left <= 0) left = 0;
+    if (left >= 360) left = 360;
+    dodger.style.left = `${left + direction}px`;
+}
